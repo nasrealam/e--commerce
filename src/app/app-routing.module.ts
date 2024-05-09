88927e7108c54870efe2d97de134eeba6066c0a2
Nasre Alam <nasrealam407@gmail.com>
2024-05-09 13:41:18 +0530

    app route module updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { DataComponent } from './body/data/data.component';
 import { BodyComponent } from './body/body.component';
 
 const routes: Routes = [
-  { path: '**', component: BodyComponent },
-  // { path: '', component: BodyComponent },
+  // { path: '**', component: BodyComponent },
+  { path: '', component: BodyComponent },
   { path: 'data', component: DataComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
